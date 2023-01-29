06976392514f8d151c8c2d37eb23c5f1ae565c71
StarGrim33 <prosky33@gmail.com>
2023-01-29 19:49:30 +0300

    Исправление замечаний

--- HomeWorkOOP11/Program.cs
+++ HomeWorkOOP11/Program.cs
@@ -6,15 +6,29 @@
         {
             Aquarium aquarium = new();
             FishBuilder fishBuilder = new();
-            aquarium.Run(fishBuilder);
+            aquarium.Run(fishBuilder, aquarium);
         }
     }
 
     class Aquarium
     {
         private FishBuilder _fishBuilder = new();
+        private List<Fish> _fishes = new();
 
-        public void Run(FishBuilder fish)
+        public Aquarium()
+        {
+            int startAmount = 10;
+
+            for(int i = 0; i < startAmount; i++)
+            {
+                _fishes.Add(_fishBuilder.CreateRandomFish());
+            }
+        }
+
+        public bool HasAliveFishes => _fishes.Count > 0;
+        public int FishesCount => _fishes.Count;
+
+        public void Run(FishBuilder fish, Aquarium aquarium)
         {
             const string CommandStartALife = "1";
             const string CommandExit = "2";
@@ -31,7 +45,7 @@
                 switch (userInput)
                 {
                     case CommandStartALife:
-                        Start(fish);
+                        Start(fish, aquarium);
                         break;
 
                     case CommandExit:
@@ -45,62 +59,12 @@
             }
         }
 
-        private void Start(FishBuilder fish)
-        {
-            int maxFishes = 10;
-            Console.WriteLine("Введите название популяции рыб: ");
-            string? userInput = Console.ReadLine();
-
-            Population population = _fishBuilder.Build(userInput, maxFishes);
-
-            Console.WriteLine("Популяция рыб сформирована, нажмите любую кнопку для продолжения");
-            Console.ReadKey();
-            Console.Clear();
-
-            population.ShowPopulation();
-            Console.WriteLine($"{new string('-', 25)}");
-
-            while(population.HasAliveFishes)
-            {
-                population.ToOldPopulation();
-                population.RemoveDead();
-
-                Console.Clear();
-                population.ShowPopulation();
-                population.AskAddFishes(fish);
-            }
-
-            if (population.HasAliveFishes == false)
-            {
-                Console.WriteLine("Все рыбки состарились и умерли");
-            }
-
-            Console.ReadKey();
-        }
-    }
-
-    class Population
-    {
-        private List<Fish> _fish = new();
-
-        public Population(string name, List<Fish> fish)
-        {
-            Name = name;
-            _fish = fish;
-            Quantity = _fish.Count;
-        }
-
-        public int Quantity { get; private set; }
-        public string Name { get; private set; }
-
-        public bool HasAliveFishes => _fish.Count > 0;
-
         public void ShowPopulation()
         {
             string fishesAlive = "Перед Вами красочный аквариум, самые красивые и необычные рыбки плавают и радуются жизни";
             string fishesAreDead = "Аквариум пуст, рыбки состарились";
 
-            if(HasAliveFishes)
+            if (HasAliveFishes)
             {
                 Console.WriteLine($"{fishesAlive}");
             }
@@ -109,16 +73,51 @@
                 Console.WriteLine($"{fishesAreDead}");
             }
 
-            for(int i = 0; i < _fish.Count; i++)
+            for (int i = 0; i < _fishes.Count; i++)
             {
-                var fish = _fish[i];
+                var fish = _fishes[i];
                 Console.WriteLine($"{i + 1} - {fish.Name}, здоровье: {fish.Health}, возраст: {fish.Age}");
             }
         }
 
-        public void ToOldPopulation()
+        public void AddFish(FishBuilder fishBuilder)
         {
-            foreach(Fish fish in _fish)
+            _fishes.Add(fishBuilder.CreateRandomFish());
+        }
+
+        public void RemoveFish(int index)
+        {
+            _fishes.RemoveAt(index - 1);
+        }
+
+        private void Start(FishBuilder fish, Aquarium aquarium)
+        {
+            User user = new();
+
+            ShowPopulation();
+            Console.WriteLine($"{new string('-', 25)}");
+
+            while(HasAliveFishes)
+            {
+                SkipDay();
+                RemoveDead();
+
+                Console.Clear();
+                ShowPopulation();
+                user.AskAddFishes(fish, aquarium);
+            }
+
+            if (HasAliveFishes == false)
+            {
+                Console.WriteLine("Все рыбки состарились и умерли");
+            }
+
+            Console.ReadKey();
+        }
+
+        private void SkipDay()
+        {
+            foreach (Fish fish in _fishes)
             {
                 fish.GrowOld();
             }
@@ -126,100 +125,35 @@
             Console.WriteLine("Одна итерация старения прошла, нажмите любую клавишу");
         }
 
-        public void RemoveDead()
+        private void RemoveDead()
         {
-            for(int i = 0; i < _fish.Count; i++)
+            for (int i = 0; i < _fishes.Count; i++)
             {
-                if (_fish[i].Health < 0)
-                    _fish.RemoveAt(i);
-            }
-        }
-
-        public void AskAddFishes(FishBuilder fishBuilder)
-        {
-            const string CommandAddFishes = "1";
-            const string CommandRemoveFishes = "2";
-            const string CommandNo = "3";
-
-            bool isUserChoice = true;
-
-            Console.WriteLine("Желаете добавить рыбку?");
-            Console.WriteLine($"{CommandAddFishes}-Добавить случайную рыбку");
-            Console.WriteLine($"{CommandRemoveFishes}-Вытащить рыбок");
-            Console.WriteLine($"{CommandNo}-Отказаться");
-
-            string? userInput = Console.ReadLine();
-
-            while(isUserChoice)
-            {
-                switch (userInput)
-                {
-                    case CommandAddFishes:
-                        _fish.Add(fishBuilder.CreateRandomFish());
-                        isUserChoice = false;
-                        break;
-
-                        case CommandRemoveFishes:
-                        RemoveFish();
-                        isUserChoice = false;
-                        break;
-
-                    case CommandNo:
-                        isUserChoice = false;
-                        break;
-
-                    default:
-                        Console.WriteLine("Нужно ввести цифру пункта меню");
-                        isUserChoice = false;
-                        break;
-                }
-            }
-        }
-
-        private void RemoveFish()
-        {
-            Console.Clear();
-            Console.WriteLine("Какую рыбку Вы хотите достать из аквариума?");
-            ShowPopulation();
-
-            bool isNumber = int.TryParse(Console.ReadLine(), out int userChoice);
-
-            if(isNumber)
-            {
-                if(userChoice <= _fish.Count && userChoice > 0)
-                {
-                    _fish.RemoveAt(userChoice - 1);
-                    Console.WriteLine($"Рыбка вынута");
-                }
-                else
-                {
-                    Console.WriteLine("Ошибка");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Нужно ввести число");
+                if (_fishes[i].Health < 0)
+                    _fishes.RemoveAt(i);
             }
         }
     }
 
     class FishBuilder
     {
-        public Population Build(string name, int quantity)
+        public Aquarium Build()
         {
-            List<Fish> fish = new List<Fish>();
+            int fishesCount = 10;
 
-            for (int i = 0; i < quantity; i++)
+            List<Fish> fish = new ();
+
+            for (int i = 0; i < fishesCount; i++)
             {
                 fish.Add(CreateRandomFish());
             }
 
-            return new Population(name, fish);
+            return new Aquarium();
         }
 
         public Fish CreateRandomFish()
         {
-            Random random = new Random();
+            Random random = new();
             var fishes = CreateFish();
             int randomIndex = random.Next(fishes.Count);
             return fishes[randomIndex];
@@ -267,7 +201,7 @@
         public override void GrowOld()
         {
             Health -= 20;
-            Age += 2;
+            Age += 1;
         }
     }
 
@@ -280,7 +214,7 @@
         public override void GrowOld()
         {
             Health -= 20;
-            Age += 3;
+            Age += 1;
         }
     }
 
@@ -293,7 +227,7 @@
         public override void GrowOld()
         {
             Health -= 30;
-            Age += 4;
+            Age += 1;
         }
     }
 
@@ -321,6 +255,96 @@
         {
             Health -= 10;
             Age += 1;
+        }
+    }
+
+    class User
+    {
+        public User()
+        {
+            Name = ToWelcome();
+        }
+
+        public string Name { get; set; }
+
+        public string ToWelcome()
+        {
+            Console.WriteLine("Как Вас зовут?");
+            string? userName = Console.ReadLine();
+
+            if(userName == null)
+            {
+                return userName = "Аноним";
+            }
+
+            return userName;
+        }
+
+        public void AskAddFishes(FishBuilder fishBuilder, Aquarium aquarium)
+        {
+            const string CommandAddFishes = "1";
+            const string CommandRemoveFishes = "2";
+            const string CommandNo = "3";
+
+            bool isUserChoice = true;
+
+            Console.WriteLine("Желаете добавить рыбку?");
+            Console.WriteLine($"{CommandAddFishes}-Добавить случайную рыбку");
+            Console.WriteLine($"{CommandRemoveFishes}-Вытащить рыбок");
+            Console.WriteLine($"{CommandNo}-Отказаться");
+
+            string? userInput = Console.ReadLine();
+
+            while (isUserChoice)
+            {
+                switch (userInput)
+                {
+                    case CommandAddFishes:
+                        aquarium.AddFish(fishBuilder);
+                        isUserChoice = false;
+                        break;
+
+                    case CommandRemoveFishes:
+                        AskRemoveFish(aquarium);
+                        isUserChoice = false;
+                        break;
+
+                    case CommandNo:
+                        isUserChoice = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Нужно ввести цифру пункта меню");
+                        isUserChoice = false;
+                        break;
+                }
+            }
+        }
+
+        public void AskRemoveFish(Aquarium aquarium)
+        {
+            Console.Clear();
+            Console.WriteLine("Какую рыбку Вы хотите достать из аквариума?");
+            aquarium.ShowPopulation();
+
+            bool isNumber = int.TryParse(Console.ReadLine(), out int userChoice);
+
+            if (isNumber)
+            {
+                if (userChoice <= aquarium.FishesCount && userChoice > 0)
+                {
+                    aquarium.RemoveFish(userChoice);
+                    Console.WriteLine($"Рыбка вынута");
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Нужно ввести число");
+            }
         }
     }
 }
